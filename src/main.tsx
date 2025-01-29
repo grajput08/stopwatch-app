16790393f30e3df3d6defe295ee01798b70b656a
Gatik Rajput <grajput2@asu.edu>
2025-01-29 19:12:19 +0530

    update: readme file

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "app/app";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Root element not found");
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
