4f169d2334c14043134d58f7ad2733cd65ae9d89
Gatik Rajput <70315319+grajput08@users.noreply.github.com>
2025-02-04 06:26:52 +0000

    Fix: remove unused data

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import useStopwatch from "./UseTimer";
 
 function AppStopWatch() {
--- src/components/TargetUI.tsx
+++ src/components/TargetUI.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const TargetUI = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-white">
